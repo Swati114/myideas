1ad5f70d8e07ba6fecbe22cd673dfba173b5e0f9
Swati <swatnb22@gmail.com>
2013-05-25 16:21:52 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myidea::Application.config.secret_token = '178bc04e73c846ccdcbc0e46acf3e75b0595f1c43cd75a532180cfaec460ad4acb9281b2afaa59931e312f7f506ab2962e432af1e48077ccec71333cbf964013'
